05cb28e358f631a8df61890a35fc359679e8f36c
kisr87 <kisr87@mail.ru>
2021-12-06 00:42:34 +0700

    DZ12

---
+++ DZ12.py
@@ -0,0 +1,29 @@
+import json
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    with open("settings.json") as f:
+        settings = json.load(f)
+    return render_template("index.html", **settings)
+
+@app.route("/candidate/<id>")
+def prof(id):
+    with open("candidates.json") as f:
+        candidates = json.load(f)
+    for candidate in candidates:
+        if candidate["id"] == int(id):
+            return render_template("candidate.html", **candidate)
+
+@app.route("/list")
+def list_candidates():
+    with open("candidates.json") as f:
+        candidates = json.load(f)
+    return render_template("list.html", users=candidates)
+
+if __name__ == "__main__":
+    app.run()
